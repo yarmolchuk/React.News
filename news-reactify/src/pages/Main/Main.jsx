fe1bc87f5cc423a82d2b73ab27eab2b6fcd61fa0
Yarmolchuk Dmytro <dmitriy.yarmolchuk@fintech-farm.com>
2025-11-06 19:32:55 +0200

    Added Skeleton component

--- news-reactify/src/pages/Main/Main.jsx
+++ news-reactify/src/pages/Main/Main.jsx
@@ -3,15 +3,19 @@ import Banner from "../../components/Banner/Banner";
 import NewsList from "../../components/NewsList/NewsList";
 import styles from "./styles.module.css";
 import { getNews } from "../../api/news";
+import Skeleton from "../../components/Skeleton/Skeleton";
 
 const Main = () => {
-    const [news, setNews] = useState([])
+    const [news, setNews] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchNews  = async () => {
             try {
-                const response = await getNews()
-                setNews(response.news)
+                setIsLoading(true)
+                const response = await getNews();
+                setNews(response.news);
+                setIsLoading(false)
             } catch (error) {
                 console.log(error);
             }
@@ -21,9 +25,17 @@ const Main = () => {
 
     return (
         <main className={styles.main}>
-            { news.length > 0 ? <Banner item = {news[0]}/> : null }
+            { news.length > 0 && !isLoading ? (
+                <Banner item = {news[0]}/> 
+            ) : ( 
+                <Skeleton type={"banner"} count={1} /> 
+            )}
 
-            <NewsList news={news}/> 
+            {!isLoading ? (
+                <NewsList news={news}/>
+            ) : (
+                <Skeleton type={"item"} count={10} />  
+            )} 
         </main>
     );
 };
